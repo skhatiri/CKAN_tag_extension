1f168ceca68073446f44d4e8889205183091d2be
Sajad <s.khatiri@gmail.com>
2020-08-14 01:05:47 +0430

    Feat calling GFbio suggest Api for tag autocomplete

--- ckanext/tag_restriction/plugin.py
+++ ckanext/tag_restriction/plugin.py
@@ -1,7 +1,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckan.logic as logic
+import requests 
+from urllib import quote_plus
+import logging
 
+
+log = logging.getLogger(__name__)
+
+#autocomplete api-endpoint 
+AUTOCOMPLETE_API = "https://terminologies.gfbio.org/api/terminologies/suggest?query={}&limit={}"
+
+#minimum tag charachters for calling autocomplete api
+AUTOCOMPLETE_MIN_CHARS = 4
 
 class Tag_RestrictionPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IActions)
@@ -9,7 +20,7 @@ class Tag_RestrictionPlugin(plugins.SingletonPlugin):
 
     def get_actions(self):
         """adds the customized tag_autocomplete action to the action chain"""
-        print("seting up action")
+        log.info("Overriding tag_autocomplte action")
         return {"tag_autocomplete":self.tag_autocomplete}
 
 
@@ -18,9 +29,18 @@ class Tag_RestrictionPlugin(plugins.SingletonPlugin):
     def tag_autocomplete(self, original_action, context, data_dict):
         """customized tag_autocomplete action, overrides default action"""
         logic.check_access('tag_autocomplete', context, data_dict)
-        return self.autocomplete_from_GFBio(data_dict)
+        return self.autocomplete_from_GFBio(data_dict['q'],data_dict['limit'])
 
 
-    def autocomplete_from_GFBio(self, tag):
+    def autocomplete_from_GFBio(self, tag, limit):
         """returns suggestions from GFBio terminologies for tags"""
-        return ["test1","test2"]
+        
+        if len(tag)<AUTOCOMPLETE_MIN_CHARS:
+            return []
+
+        encoded_url = AUTOCOMPLETE_API.format(quote_plus(tag), limit)
+        r = requests.get(encoded_url)
+        response = r.json()
+        if response['results']:
+            return [result['label'] for result in response['results'] ]
+        return []
