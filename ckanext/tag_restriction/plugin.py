cf0a5e85594bd7d1b1e6d26c03dd0f8f68cfe87e
Sajad <s.khatiri@gmail.com>
2020-08-15 08:13:38 +0430

    Feat exception handling on api calls

--- ckanext/tag_restriction/plugin.py
+++ ckanext/tag_restriction/plugin.py
@@ -10,27 +10,27 @@ log = logging.getLogger(__name__)
 
 #autocomplete api-endpoint
 AUTOCOMPLETE_API = config.get('ckan.tag_restriction.autocomplete_api',
-                               '#https://terminologies.gfbio.org/api/terminologies/suggest?query={}&limit={}')
+                               'https://terminologies.gfbio.org/api/terminologies/suggest?query={}&limit={}')
 
 #search api-endpoint
 SEARCH_API = config.get('ckan.tag_restriction.search_api',
-                        '#https://terminologies.gfbio.org/api/terminologies/search?query={}')
+                        'https://terminologies.gfbio.org/api/terminologies/search?query={}')
 
 #minimum tag charachters for calling autocomplete api
 AUTOCOMPLETE_MIN_CHARS = toolkit.asint(config.get('ckan.tag_restriction.autocomplete_min_chars',
-                                   14))
+                                   4))
 
 #autocomplete results filed in api response
 AUTOCOMPLETE_RESULT_FIELD = config.get('ckan.tag_restriction.autocomplete_results_field',
-                                       '#results')
+                                       'results')
 
 #autocomplete tag label filed in api response
 AUTOCOMPLETE_LABEL_FIELD = config.get('ckan.tag_restriction.autocomplete_label_field',
-                                      '#label')
+                                      'label')
 
 #search results filed in api response
 SEARCH_RESULT_FIELD = config.get('ckan.tag_restriction.search_results_field',
-                                 '#results')
+                                 'results')
 
 
 class Tag_RestrictionPlugin(plugins.SingletonPlugin):
@@ -40,14 +40,14 @@ class Tag_RestrictionPlugin(plugins.SingletonPlugin):
 
     def get_validators(self):
         """adds the customized tag_name_validator to known validators"""
-        log.info("Overriding default tag_name_validator")
+        log.info('Overriding default tag_name_validator')
         return {u'tag_name_validator':self.tag_name_validator}
 
 
     def get_actions(self):
         """adds the customized tag_autocomplete action to the action chain"""
-        log.info("Overriding tag_autocomplte action")
-        return {"tag_autocomplete":self.tag_autocomplete}
+        log.info('Overriding tag_autocomplte action')
+        return {'tag_autocomplete':self.tag_autocomplete}
 
 
     @logic.side_effect_free
@@ -63,8 +63,11 @@ class Tag_RestrictionPlugin(plugins.SingletonPlugin):
         #calling default validator
         value = logic.validators.tag_name_validator(value,context)
 
-        if not self.is_in_tag_API(value):
-            raise toolkit.Invalid('\"{}\" is not in valid tags resource'.format(value))
+        try:
+            if not self.is_in_tag_API(value):
+                raise toolkit.Invalid('\"{}\" is not in valid tags resource'.format(value))
+        except:
+            raise toolkit.Invalid('error while checking tag validity, contact website administrator')
         return value
 
         
@@ -75,10 +78,15 @@ class Tag_RestrictionPlugin(plugins.SingletonPlugin):
             return []
 
         encoded_url = AUTOCOMPLETE_API.format(quote_plus(tag), limit)
-        r = requests.get(encoded_url)
-        response = r.json()
-        if response[AUTOCOMPLETE_RESULT_FIELD]:
-            return [result[AUTOCOMPLETE_LABEL_FIELD] for result in response[AUTOCOMPLETE_RESULT_FIELD] ]
+        try:
+            r = requests.get(encoded_url)
+            response = r.json()
+            if response[AUTOCOMPLETE_RESULT_FIELD]:
+                return [result[AUTOCOMPLETE_LABEL_FIELD] for result in response[AUTOCOMPLETE_RESULT_FIELD] ]
+        except requests.exceptions.RequestException as e:
+            log.error('exception when calling tag autocomplete api')
+            log.error(e)
+            return ['sugestions not available']
         return []
 
     
@@ -86,9 +94,14 @@ class Tag_RestrictionPlugin(plugins.SingletonPlugin):
         """checks whether the tag exists provided api terminologies"""
 
         encoded_url = SEARCH_API.format(quote_plus(tag))
-        r = requests.get(encoded_url)
-        response = r.json()
-        if response[SEARCH_RESULT_FIELD] and len(response[SEARCH_RESULT_FIELD]) > 0 :
-            return True
-        return False
+        try:
+            r = requests.get(encoded_url)
+            response = r.json()
+            if response[SEARCH_RESULT_FIELD] and len(response[SEARCH_RESULT_FIELD]) > 0 :
+                return True
+            return False
+        except requests.exception.requestException as e:
+            log.error('exception when calling tag search api')
+            log.error(e)
+            raise e
 
